e4078f78210429fb42029fe8ae5660aef5f0eaa0
Challelign Tilahun <chalie2123@gmail.com>
2024-02-07 16:38:43 +0300

    Initial commit

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+			<p className="text-3xl font-medium text-sky-700">
+				<Button variant="destructive">Click me</Button>
+			</p>
+		</div>
+	);
+}
